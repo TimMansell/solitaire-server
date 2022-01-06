4bffbda26394f39243785c281af908cb0d53a4d8
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-01-06 15:02:10 +1100

    remove deprecated jest config settings

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
-  snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$':
-      'jest-transform-stub',
   },
   transformIgnorePatterns: ['/node_modules/'],
 };
