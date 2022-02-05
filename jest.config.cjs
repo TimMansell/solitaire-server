e4cc19fa7aa532b3fbad8913340c32efed47fd37
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-02-05 22:00:38 +1100

    refactor jest to work in esm environment

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^#@/(.*)$': '<rootDir>/src/$1',
   },
   modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
   testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
