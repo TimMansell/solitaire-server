f0efe93f0e4e0d02fcd0a0380e06f2ff0703c9dc
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-02-18 15:39:52 +1100

    remove all graphql code

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { setupExpress, setupDB } from './setup';
 import { setupSockets } from './sockets';
-import { setupGraphQl } from './graphql';
 
 const main = async () => {
   const [express, db] = await Promise.all([setupExpress(), setupDB()]);
 
   setupSockets(express, db);
-  setupGraphQl(express);
 };
 
 main();
