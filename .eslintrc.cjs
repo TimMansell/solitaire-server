3b4d810d120cffb9d1aa79b63a5693250cb49493
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-03-02 16:57:26 +1100

    migrate cypress eslint file to main eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,20 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    mocha: true,
+    'cypress/globals': true,
   },
   extends: [
     'airbnb-base',
     'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
     'plugin:security/recommended',
     'plugin:node/recommended',
   ],
-  plugins: ['jest', 'prettier', 'security'],
+  plugins: ['jest', 'prettier', 'cypress', 'security'],
   parserOptions: {
     ecmaVersion: 2020,
   },
-  ignorePatterns: ['dist/'],
   rules: {
     'import/no-cycle': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
