02b452f2862b78196bba1c74480663f207dc1376
TimMansell <1926461+TimMansell@users.noreply.github.com>
2022-03-31 22:15:56 +1100

    refactor code to check if version is outdated on server

--- src/sockets/version.js
+++ src/sockets/version.js
@@ -1,8 +1,11 @@
+import semver from 'semver';
 import { getVersion } from '#@/db/version';
 
 // eslint-disable-next-line import/prefer-default-export
-export const checkVersion = ({ socket }) => {
-  const version = getVersion();
+export const checkVersion = async ({ socket, db, version }) => {
+  const latestVersion = await getVersion({ db });
+  const isValid = semver.valid(version);
+  const isOutdated = !isValid || !semver.eq(version, latestVersion);
 
-  socket.emit('checkVersion', version);
+  socket.emit('setVersion', { latestVersion, isOutdated });
 };
